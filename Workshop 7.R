9a87f4900c891711d6679f5b22c4184499a78c5b
Naveen Works <naveen.manoharan93@gmail.com>
2022-03-15 19:03:58 +1100

    workshop 7

---
+++ Workshop 7.R
@@ -0,0 +1,7 @@
+library(tidyverse)
+library(skimr)
+
+
+coffee_ratings <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-07-07/coffee_ratings.csv')
+
+
